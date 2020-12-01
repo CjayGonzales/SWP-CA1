bbd77d74a362246561fc5d453693754dcbfc476e
Clemente Gonzales <cjaygonzales117@gmail.com>
2020-12-01 12:26:44 +0000

    deleted

--- assets/tilemaps/castle_Tilesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="castle_tilesheet" tilewidth="16" tileheight="16" tilecount="128" columns="8">
- <image source="castle-tileset.png" width="128" height="256"/>
-</tileset>
